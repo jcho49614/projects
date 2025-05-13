043e8a9b2e19ff8dc631a92416bdc38d534633f2
JamesCho <jcho49614@gmail.com>
2025-05-13 11:57:27 +0900

    051325 section 1 image checker done

--- project3/prototype.py
+++ project3/prototype.py
@@ -1,8 +1,22 @@
 import tensorflow as tf
 import os
+import cv2
+import imghdr
 
 testing = 'data'
 valid_exts = ['jpg', 'jpeg', 'png']
-for image_class in os.path.join(testing):
-    for image in os.path.join(testing, image_class):
-        print(image)
+
+for image_class in os.listdir(testing):
+    classdir = os.path.join(testing, image_class)
+    for image in os.listdir(classdir):
+        imagedir = os.path.join(classdir, image)
+        image = cv2.imread(imagedir)
+        extension = imghdr.what(imagedir)
+        if extension not in valid_exts:
+            print("Image {} is not valid!", format(imagedir))
+        elif image is None:
+            print("Image {} is not valid!", format(imagedir))
+        else:
+            print("Image {} is valid! Using now.".format(imagedir)) 
+
+
